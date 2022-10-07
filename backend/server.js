1c543b91865bba0e5c6eb7d90844d84acaff7c9c
Nik-portfolio <kravtcovmikita@gmail.com>
2022-10-07 19:12:59 +0300

    feat: create basic API

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 "use strict";
+const chats  = require('./data/data');
 const express = require('express');
+require('dotenv').config()
 const app = express();
+
 app.get('/', (req, res) => {
     res.send('API is Running');
 });
-app.listen(5000, () => {
-    console.log('Server run on 5000....');
-});
+
+app.get('/api/chat', (req, res) => {
+    res.json({res: chats})
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const { id } = req.params;
+    const searchingChat = chats.find(chat => chat._id === id) 
+    res.send(searchingChat)
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.get(
+app.listen(PORT, () => {
+    console.log(`Server run on ${PORT}....`);
+}));
